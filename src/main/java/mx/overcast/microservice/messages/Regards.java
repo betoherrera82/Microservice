656baa86b3c5f1270f9a5c718be9806da32c609f
Carlos Herrera <carlos.herrera@adira.com.mx>
2018-06-13 08:52:29 -0500

    Correccion de bug de documentacion.

--- src/main/java/mx/overcast/microservice/messages/Regards.java
+++ src/main/java/mx/overcast/microservice/messages/Regards.java
@@ -4,7 +4,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(
-		value = "User",
+		value = "Regards",
 		description = "Objeto que representa al saludo."
 		)
 public class Regards {
