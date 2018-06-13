f6052f2a52b8d9999641de49ca87acb1c8f3b8ee
Carlos Herrera <carlos.herrera@adira.com.mx>
2018-06-13 08:21:34 -0500

    Se documenta la API de Swagger.

--- src/main/java/mx/overcast/microservice/endpoints/Endpoint.java
+++ src/main/java/mx/overcast/microservice/endpoints/Endpoint.java
@@ -8,9 +8,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import mx.overcast.microservice.messages.Message;
 import mx.overcast.microservice.services.RegardsService;
 
+@Api(
+		description = "API de Saludo Overcast",
+		tags = "Regards Endpoint"
+		)
 @RestController
 @RequestMapping(
 		path = "/microservice"
@@ -20,6 +26,11 @@ public class Endpoint {
 	@Autowired
 	private RegardsService regardsService;
 
+	@ApiOperation(
+			value = "Envia un saludo a un usuario.",
+			notes = "Envia un saludo a un usuario. Revisar el modelo para determinar los campos requeridos.",
+			nickname = "regards"
+			)
 	@RequestMapping(
 			method = RequestMethod.POST,
 			produces = MediaType.APPLICATION_JSON_VALUE,
